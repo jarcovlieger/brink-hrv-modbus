97f95138f44c2280af7b5c06b952b8005337aef8
Jarco Vlieger <jarcovlieger@solcon.nl>
2026-06-04 11:42:54 +0200

    Added filter used in hours

--- custom_components/brink_ventilation/sensor.py
+++ custom_components/brink_ventilation/sensor.py
@@ -3,6 +3,8 @@
 from __future__ import annotations
 
 import logging
+
+from custom_components.brink_ventilation.sensors.filter_used_in_hours_sensor import FilterUsedInHoursSensor
 from .sensors.supply_temperature_sensor import SupplyTemperatureSensor
 from .sensors.exhaust_temperature_sensor import ExhaustTemperatureSensor 
 from .const import DOMAIN
@@ -13,6 +15,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities([
         SupplyTemperatureSensor(coordinator, entry.entry_id),
-        ExhaustTemperatureSensor(coordinator, entry.entry_id)
+        ExhaustTemperatureSensor(coordinator, entry.entry_id),
+        FilterUsedInHoursSensor(coordinator, entry.entry_id)
         ])
 
